7adf724e8cd0aca2b6effa0353a77625920772fc
steinjun0 <1142308@naver.com>
2021-09-20 17:38:29 +0900

    코드정리 * 함수형태로 중복제거 * Promise로 비동기 정리

--- src/js/threeRendering.js
+++ src/js/threeRendering.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { Vector3 } from "three";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 
 const renderer = new THREE.WebGLRenderer();
@@ -6,56 +7,163 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-const PerspectiveCamera = new THREE.PerspectiveCamera(
+const camera = new THREE.PerspectiveCamera(
   90, // 시야각 (FOV, Field Of View)
   window.innerWidth / window.innerHeight, // 종횡비
   // near, far 절단면은 렌더링 거리를 의미한다
   0.1, // near 절단면
   1000 // far 절단면
 );
-PerspectiveCamera.position.set(0, 0, 20);
-PerspectiveCamera.lookAt(0, 0, 0);
+// camera.position.set(eyesCenterPos.x, eyesCenterPos.y, eyesCenterPos.z);
+camera.position.set(5, 5, 5);
+camera.lookAt(0, 0, 0);
 
-const material = new THREE.LineBasicMaterial({ color: 0x00ffff });
-const points = [];
-points.push(new THREE.Vector3(-10, 0, 0));
-points.push(new THREE.Vector3(0, 10, 0));
-points.push(new THREE.Vector3(10, 0, 0));
+function getLines(){
+  const material = new THREE.MeshPhongMaterial({ color: 0x00ffff });
+  const points = [];
+  points.push(new THREE.Vector3(-10, 0, 0));
+  points.push(new THREE.Vector3(0, 10, 0));
+  points.push(new THREE.Vector3(10, 0, 0));
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+  const line = new THREE.Line(geometry, material);
+  return line
+}
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
-const line = new THREE.Line(geometry, material);
-// console.log(line);
+function getBox(width, height, depth, x, y, z, color=0x44aa88){
+  const boxGeometry = new THREE.BoxGeometry(width, height, depth);
+  const boxMaterial = new THREE.MeshPhongMaterial({ color: color });
+  const cube = new THREE.Mesh(boxGeometry, boxMaterial);
+  cube.position.set(x, y, z);
+  return cube
+}
 
-scene.add(line);
-// const loader = new GLTFLoader();
+function getXYZBias(){
+  const cylinderXGeometry = new THREE.CylinderGeometry(0.1, 0.1, 100, 32);
+  const cylinderXMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const cylinderX = new THREE.Mesh(cylinderXGeometry, cylinderXMaterial);
+  cylinderX.rotation.z = Math.PI / 2;
+  cylinderX.position.set(0, 0, 0);
 
-// instantiate a loader
+  const cylinderYGeometry = new THREE.CylinderGeometry(0.1, 0.1, 100, 32);
+  const cylinderYMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cylinderY = new THREE.Mesh(cylinderYGeometry, cylinderYMaterial);
+  cylinderY.rotation.x = Math.PI / 2;
+  // cylinderY.position.set(0, 0, 0);
+
+  const cylinderZGeometry = new THREE.CylinderGeometry(0.1, 0.1, 100, 32);
+  const cylinderZMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+  const cylinderZ = new THREE.Mesh(cylinderZGeometry, cylinderZMaterial);
+  // cylinderZ.rotation.x = 90;
+  // cylinderZ.rotation.z = 90;
+  return [cylinderX, cylinderY, cylinderZ]
+}
+
+const cube = getBox(3,3,3, 0,0,0)
+const [cylinderX, cylinderY, cylinderZ] = getXYZBias()
+
+scene.add(cube);
+
+scene.add(cylinderX);
+scene.add(cylinderY);
+scene.add(cylinderZ);
+
+
+
 const loader = new OBJLoader();
 
 const color = 0xffffff;
 const intensity = 1;
-const light = new THREE.AmbientLight(color, intensity);
-
+// const light = new THREE.AmbientLight(color, intensity);
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
 scene.add(light);
-// load a resource
-loader.load(
-  // resource URL
-  "FinalBaseMesh.obj",
-  // called when resource is loaded
-  function (object) {
-    scene.add(object);
-    console.log("added");
-  },
-  // called when loading is in progresses
-  function (xhr) {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  },
-  // called when loading has errors
-  function (error) {
-    console.log("An error happened");
-  }
-);
 
-renderer.render(scene, PerspectiveCamera);
+
+function getLoadOBJPromise(loader, filename){
+  let promise = new Promise(
+    function(resolve, reject){
+      loader.load(
+        // resource URL
+        filename,
+        // called when resource is loaded
+        function (object) {
+          // scene.add(object);
+          // objectRes = object;
+          resolve(object)
+        },
+        // called when loading is in progresses
+        function (xhr) {
+          console.log(filename + ":" + (xhr.loaded / xhr.total) * 100 + "% loaded");
+        },
+        // called when loading has errors
+        function (error) {
+          console.log("An error happened");
+          reject(error)
+        }
+      );
+    }
+  )
+  return promise
+}
+
+let room
+let roomPromise = getLoadOBJPromise(loader, "room.obj")
+roomPromise.then(
+  function(objectRes){
+    room = objectRes
+    scene.add(objectRes)
+  }
+)
+.catch(
+  function(error){
+    console.log(error)
+  }
+)
+
+let man
+let manPromise = getLoadOBJPromise(loader, "FinalBaseMesh.obj")
+manPromise.then(
+  function(objectRes){
+    man = objectRes
+    console.log('man',man)
+    man.position.set(-5, -5, -5)
+    scene.add(objectRes)
+  }
+)
+.catch(
+  function(error){
+    console.log(error)
+  }
+)
+
+
+renderer.render(scene, camera);
+
+function render(time) {
+  time *= 0.001; // convert time to seconds
+  cube.rotation.x = 0;
+  cube.rotation.y = 0;
+  // console.log(
+  //   "eyesCenterPos.x, eyesCenterPos.y, eyesCenterPos.z",
+  //   eyesCenterPos.x,
+  //   eyesCenterPos.y,
+  //   eyesCenterPos.z
+  // );
+  // camera.position.set(
+  //   (0.5 - eyesCenterPos.x) * 5,
+  //   (0.5 - eyesCenterPos.y) * 5 + 20,
+  //   eyesCenterPos.z * 200 + 20
+  // );
+  // camera.position.set(10, 10, 10);
+  try {
+    // man.rotation.x = time;
+    // man.rotation.y = time;
+  } catch (error) {}
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
 
 
